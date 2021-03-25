6efeaa3dafac4a5e0bef8736d0bb668349838233
Eduardo Tanabe <e_tanabe@yahoo.com.br>
2021-03-24 20:17:10 -0400

    Atividade avaliativa - Aula 03

---
+++ modulo-introducao/src/main/java/com/ifms/ModuloIntroducaoApplication.java
@@ -0,0 +1,13 @@
+package com.ifms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloIntroducaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloIntroducaoApplication.class, args);
+	}
+
+}
