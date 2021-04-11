7408ade6de0deae6957893594d823969b547c057
Eduardo Tanabe <e_tanabe@yahoo.com.br>
2021-04-10 21:48:32 -0400

    Aplicação do CRUD para Modalidade

---
+++ modulo-introducao/src/main/resources/data.sql
@@ -0,0 +1,30 @@
+INSERT INTO modalidade (descricao)
+
+VALUES
+
+('Atletismo'),
+
+('Badminton'),
+
+('Basquete'),
+
+('Ciclismo'),
+
+('Futsal'),
+
+('Ginástica Rítmica'),
+
+('Handebol'),
+
+('Judô'),
+
+('Natação'),
+
+('Tênis de Mesa'),
+
+('Vôlei'),
+
+('Vôlei de Praia'),
+
+('Wrestling');
+
